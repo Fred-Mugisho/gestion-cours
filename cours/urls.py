a52a7b959083612487357d6c2c24dcfc59f43e2f
Fred-Mugisho <frederickmugisho228@gmail.com>
2024-09-04 06:06:10 +0200

    Ajout pages de base
    Signed-off-by: Fred-Mugisho <frederickmugisho228@gmail.com>

--- cours/urls.py
+++ cours/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'cours'
 urlpatterns = [
     path('', views.home, name='home'),
+    path('login/', views.login_system, name='login'),
+    path('register/', views.register, name='register'),
+    path('dashboard/', views.dashboard, name='dashboard'),
 ]
--- cours/views.py
+++ cours/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -8,3 +8,16 @@ def home(request):
         'nom': nom
     }
     return render(request, 'cours/home.html', context)
+
+def login_system(request):
+    if request.method == 'POST':
+        print(request.POST)
+        return redirect('cours:dashboard')
+    
+    return render(request, 'cours/login.html')
+
+def register(request):
+    return render(request, 'cours/register.html')
+
+def dashboard(request):
+    return render(request, 'cours/dashboard.html')
